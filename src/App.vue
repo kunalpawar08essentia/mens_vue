b9920e4b81f290854252237297d707af105df368
kunalpawar08essentia <kunal.pawar@essentia.dev>
2021-10-07 21:49:50 +0530

    Added View Product and addtocart option

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,46 @@
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>Men's Accessories</strong></router-link>
+        <router-link to="/" class="navbar-item"
+          ><strong>Men's Accessories</strong></router-link
+        >
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div
+        class="navbar-menu"
+        id="navbar-menu"
+        v-bind:class="{ 'is-active': showMobileMenu }"
+      >
         <div class="navbar-start">
           <div class="navbar-item">
             <form method="get" action="/search">
               <div class="field has-addons">
                 <div class="control">
-                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                  <input
+                    type="text"
+                    class="input"
+                    placeholder="What are you looking for?"
+                    name="query"
+                  />
                 </div>
 
                 <div class="control">
                   <button class="button is-success">
-                      <span class="icon">
+                    <span class="icon">
                       <i class="fas fa-search"></i>
-                      </span>
+                    </span>
                   </button>
                 </div>
               </div>
@@ -41,17 +58,17 @@
 
           <div class="navbar-item">
             <div class="buttons">
-              
-                <router-link to="/my-account" class="button is-light">My account</router-link>
-            
+              <router-link to="/my-account" class="button is-light"
+                >My account</router-link
+              >
 
-              
-                <router-link to="/log-in" class="button is-light">Log in</router-link>
-            
+              <router-link to="/log-in" class="button is-light"
+                >Log in</router-link
+              >
 
               <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart</span>
+                <span>Cart ({{ cartTotalLength }})</span>
               </router-link>
             </div>
           </div>
@@ -64,15 +81,46 @@
     </div>
 
     <section class="section">
-      <router-view/>
+      <router-view />
     </section>
 
     <footer class="footer">
       <p class="has-text-centered">Copyright (c) 2021</p>
+
+      <p class="has-text-right">Designed by Kunal Pawar</p>
     </footer>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+      cart: {
+        items: [],
+      },
+    };
+  },
+  beforeCreate() {
+    this.$store.commit("initializeStore");
+  },
+  mounted() {
+    this.cart = this.$store.state.cart;
+  },
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0;
+
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity;
+      }
+
+      return totalLength;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,76 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome to the Online Store</p>
+        <p class="subtitle">The best Men's Accessories</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail"/>
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">₹{{ product.price }}</p>
+          
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import axios from 'axios'
 export default {
-  name: 'Home',
-  components: {
-   
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
+  components: {},
+  mounted() {
+    this.getLatestProducts();
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get("/api/v1/latest-products/")
+        .then((response) => {
+          this.latestProducts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
+
+
+<style scoped>
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+.hero {
+  margin-left: -2.97rem;
+  margin-right: -2.97rem;
+  margin-top: -2.97rem;
+  background: url('https://images.unsplash.com/photo-1633574364467-808a45292353?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80');
+}
+</style>
