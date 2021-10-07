a01a1c33af7e3e0994a81ee2c28350dd592bd4d2
kunalpawar08essentia <kunal.pawar@essentia.dev>
2021-10-07 13:04:05 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Men's Accessories</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="What are you looking for?" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                      <span class="icon">
+                      <i class="fas fa-search"></i>
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <router-link to="/shirts" class="navbar-item">T-shirts</router-link>
+          <router-link to="/jeans" class="navbar-item">Jeans</router-link>
+          <router-link to="/shoes" class="navbar-item">Shoes</router-link>
+          <router-link to="/wallet" class="navbar-item">Wallet</router-link>
+          <router-link to="/belt" class="navbar-item">Belt</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              
+                <router-link to="/my-account" class="button is-light">My account</router-link>
+            
+
+              
+                <router-link to="/log-in" class="button is-light">Log in</router-link>
+            
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="is-loading-bar has-text-centered">
+      <div class="lds-dual-ring"></div>
+    </div>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
   </div>
-  <router-view/>
 </template>
 
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "../node_modules/bulma";
 </style>
